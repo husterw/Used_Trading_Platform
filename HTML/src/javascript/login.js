b68a73b69e0be54dd1b18c49c899b43d843373b3
husterw <U202215428@hust.edu.cn>
2024-09-29 20:46:07 +0800

    update login page

--- HTML/src/javascript/login.js
+++ HTML/src/javascript/login.js
@@ -28,3 +28,22 @@ document.querySelectorAll(".login-info > a").forEach((link) => {
     }
   });
 });
+
+document
+  .querySelector(".register-link")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".login-img").classList.add("move-right");
+    document.querySelector(".login-info-container").classList.add("fade-out");
+    document.querySelector(".register-info-container").classList.add("fade-in");
+  });
+
+document.querySelector(".login-link").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("click");
+  document.querySelector(".login-img").classList.remove("move-right");
+  document.querySelector(".login-info-container").classList.remove("fade-out");
+  document
+    .querySelector(".register-info-container")
+    .classList.remove("fade-in");
+});
