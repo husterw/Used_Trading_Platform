effbfa6181de20f5b99bc5469b595ca03826ec41
wsh <3358646384@qq.com>
2024-10-08 21:55:51 +0800

    yy

--- HTML/src/javascript/login.js
+++ HTML/src/javascript/login.js
@@ -49,31 +49,35 @@ document.querySelector(".login-link").addEventListener("click", function (e) {
     .classList.remove("fade-in");
 });
 
-// 处理登录表单提交
-document
-  .querySelector("form[name='plf']")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
+// 处理密码登录表单提交
+document.querySelectorAll("form").forEach((form) => {
+  if (form.name === "plf" || form.name === "mlf") {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
 
-    const form = e.target;
-    const formData = new FormData(form);
+      const Form = e.target;
+      const formData = new FormData(Form);
+      const type = Form.name === "plf" ? "password" : "email";
+      formData.append("type", type);
 
-    fetch(form.action, {
-      method: form.method,
-      body: new URLSearchParams(formData),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status === "success") {
-          window.location.href = "../html/news.html";
-        } else {
-          alert(data.message);
-        }
+      fetch(Form.action, {
+        method: form.method,
+        body: new URLSearchParams(formData),
       })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === "success") {
+            window.location.href = "../html/news.html";
+          } else {
+            alert(data.message);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  }
+});
 
 // 控制表单切换时的数据存留
 document.querySelectorAll("form").forEach((form) => {
@@ -145,3 +149,40 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1000);
   }
 });
+
+// 处理注册表单
+document
+  .querySelector("form[name='registerForm']")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const FORM = e.target;
+    const formData = new FormData(FORM);
+
+    const firstPassword = formData.get("password");
+    const secondPassword = formData.get("confirmPassword");
+
+    if (firstPassword !== secondPassword) {
+      alert("两次输入的密码不一致");
+      return;
+    }
+
+    fetch(FORM.action, {
+      method: FORM.method,
+      body: new URLSearchParams(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          alert("注册成功");
+          location.reload();
+        } else if (data.status === "already") {
+          alert("该邮箱已被注册");
+        } else {
+          alert("注册失败");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
