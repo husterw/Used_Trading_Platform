0e842ec9672f3f2eb5bd29a5bd57e2a248f6ade0
Merlin0224 <3345967904@qq.com>
2024-10-18 14:11:48 +0800

    index

---
+++ HTML/src/javascript/index.js
@@ -0,0 +1,9 @@
+const gotoMessagePage = document.querySelector(".bx-comment-dots");
+gotoMessagePage.addEventListener("click", function () {
+  window.location.href = "news.html";
+});
+
+const gotoLoginPage = document.querySelector(".bx-log-out");
+gotoLoginPage.addEventListener("click", function () {
+  window.location.href = "login.html";
+});
