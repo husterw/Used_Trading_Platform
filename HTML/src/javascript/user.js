419f7a02f0822ec28f02c309935e5b2715fb5433
husterw <U202215428@hust.edu.cn>
2024-10-23 22:32:28 +0800

    update user pages

--- HTML/src/javascript/user.js
+++ HTML/src/javascript/user.js
@@ -17,3 +17,19 @@ const gotoShopCart = document.querySelector(".bx-cart");
 gotoShopCart.addEventListener("click", function () {
   window.location.href = "shopcart.html";
 });
+
+document.querySelector(".profile-edit").addEventListener("click", function (e) {
+  e.preventDefault();
+  const informationForm = document.querySelector(".information-form");
+  const overlay = document.querySelector(".overlay");
+
+  console.log(informationForm);
+
+  informationForm.classList.add("show");
+  overlay.classList.add("show");
+
+  overlay.addEventListener("click", function () {
+    informationForm.classList.remove("show");
+    overlay.classList.remove("show");
+  });
+});
