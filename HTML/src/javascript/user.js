fef3e89b035293bebe670aa2390036dac81b49d3
husterw <U202215428@hust.edu.cn>
2024-10-25 19:17:32 +0800

    user

--- HTML/src/javascript/user.js
+++ HTML/src/javascript/user.js
@@ -1,4 +1,9 @@
-// 初始化
+//初始化个人信息页面
+
+//页面加载时获取用户信息
+document.addEventListener("DOMContentLoaded", (event) => {
+  loadUserInfo(); //初始化用户信息
+});
 
 const gotoMessagePage = document.querySelector(".bx-comment-dots");
 gotoMessagePage.addEventListener("click", function () {
@@ -61,7 +66,6 @@ document
           document.querySelector(".user-intro").innerHTML = userIntroduction;
           document.querySelector(".information-form").classList.remove("show");
           document.querySelector(".overlay").classList.remove("show");
-          window.location.reload();
         }
       })
       .catch((error) => {
@@ -90,4 +94,20 @@ function showAlert(message) {
       document.body.removeChild(alertBox);
     }, 300);
   }, 2000);
+}
+
+//加载用户信息的函数
+function loadUserInfo() {
+  const userId = localStorage.getItem("userid");
+
+  fetch(`http://localhost:2999/user/info/${userId}`)
+    .then((response) => response.json())
+    .then((userInfo) => {
+      //更新用户信息显示
+      document.getElementById("userName").innerText = userInfo.uname;
+      document.getElementById("userPhone").innerText = userInfo.phone;
+      document.getElementById("userAddress").innerText = userInfo.dormitory;
+      document.getElementById("userIntroduction").innerText = userInfo.remark;
+    })
+    .catch((error) => console.error("Error fetching user info:", error));
 }
